b1c9d0f3156380c4e1e3574634d176f3acb8a3ca
mehmetkodtepe <mehmet@kodtepe.com>
2025-12-28 17:27:30 +0300

    edge added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-import data from "../data/cafes.json"
+export const runtime = "edge";
 
 import CaffeCard from "../components/CaffeCard";
 
